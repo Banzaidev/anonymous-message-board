1d398e60e70b27bf1669bfa8a2d300d6a9cecacf
Banzaidev <66788936+Banzaidev@users.noreply.github.com>
2025-06-17 20:24:01 +0200

    fix: set default value for created_on in repliesSchema

--- schemas/replies.schema.js
+++ schemas/replies.schema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const repliesSchema = new mongoose.Schema({
     text: {type: String, required: true},
-    created_on: {type: Date, default: () =>  new Date(Date.now()).toUTCString()},
+    created_on: {type: Date, default: () =>  new Date()},
     reported: {type:Boolean, default: false},
     delete_password: {type:String, required: true},
 })
