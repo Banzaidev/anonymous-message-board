1bea438e4c6b672a5034108d1f230fd6d9c3558b
Banzaidev <66788936+Banzaidev@users.noreply.github.com>
2025-06-17 12:33:07 +0200

    feat: update thread schema with board, replycount, and proper date defaults

--- schemas/threads.schema.js
+++ schemas/threads.schema.js
@@ -3,11 +3,13 @@ const repliesSchema = require('./replies.schema')
 
 const threadsSchema = new mongoose.Schema({
     text: {type: String, required: true},
-    created_on: {type: Date, default: () =>  new Date(Date.now()).toUTCString()},
-    bumped_on: {type: Date, default: () =>  new Date(Date.now()).toUTCString()},
+    board: {type:String, required:true},
+    created_on: {type: Date, default: () => new Date()},
+    bumped_on: {type: Date, default: () => new Date()},
     reported: {type: Boolean, default: false},
     delete_password: {type: String, required:true},
-    replies: {type: [repliesSchema], default: []}
+    replies: {type: [repliesSchema], default: []},
+    replycount: {type: Number, default: 0}
 })
 
 module.exports = threadsSchema
